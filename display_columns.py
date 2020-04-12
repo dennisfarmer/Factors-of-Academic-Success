56c79c7b6fa22cc5c6040c7f4a6996e0865e76cc
Dennis Farmer <56651870+dfarmer3@users.noreply.github.com>
2020-04-12 16:42:34 -0400

    Updated display_columns module

--- display_columns.py
+++ display_columns.py
@@ -57,22 +57,22 @@ def divide_dataframe(df:"pandas.DataFrame, pandas.Series", display_columns:"int"
     # Create list of DataFrames, where each DataFrame is a slice of the original DataFrame
     split_dataframe = [df[r[i]:r[i+1]+1] if i == display_columns-1 else df[r[i]:r[i+1]] for i in range(len(r)-1)]
     
-    html_str=''
+    strHtml = ''
     for x in split_dataframe:
-        html_str += x.to_html()
-    display_html(html_str.replace('table','table style="display:inline"'), raw=True)
+        strHtml += x.to_html()
+    display_html(strHtml.replace('table','table style="display:inline"'), raw=True)
 
 
 
-def display_side_by_side(*args:"pandas.DataFrame, pandas.Series", drop_index:"bool"=True)-> "None":
+def display_side_by_side(*args:"pandas.DataFrame, pandas.Series", drop_index:"bool"=False)-> "None":
     """Display multiple DataFrames side-by-side in a Jupiter Notebook.
     
     Parameters
     ----------
     *args : pandas.DataFrame or pandas.Series
         
-    drop_index : bool, default True
-        Determines whether to reset the original index. By default, the index is reset to integer values.
+    drop_index : bool, default False
+        Determines whether to reset the index of each DataFrame. By default, all indexes are kept.
     
     Returns
     -------
@@ -81,17 +81,22 @@ def display_side_by_side(*args:"pandas.DataFrame, pandas.Series", drop_index:"bo
     """
     from IPython.display import display_html
     
-    html_str=''
+    strHtml = ''
     for df in args:
+        
         if isinstance(df, pandas.Series):
             df = df.to_frame()
-        html_str+=df.to_html()
-    display_html(html_str.replace('table','table style="display:inline"'), raw=True)
+        if drop_index:
+            df.reset_index(drop=True, inplace=True)
+            
+        strHtml += df.to_html()
+    display_html(strHtml.replace('table','table style="display:inline"'), raw = True)
     
 
 
 if __name__ == "__main__":
     print("Charlie don’t surf!",
+          "https://www.youtube.com/watch?v=yVcG0cTtSVo",
           "https://open.spotify.com/album/2UxN3UKyS3Z5r0Sra8A5RF?highlight=spotify:track:5T4j3Uv33fQO7tsR2lMl4y",
           sep="\n\n")
     
